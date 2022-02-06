a2f631710ef7ae4fdf7e8b19f71ec6bb690d561b
bribricot <kraemer.lucie@gmail.com>
2022-02-07 00:14:24 +0100

    Using Functional Components

--- script.jsx
+++ script.jsx
@@ -1,6 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+let header = (
+	<h1>Hello</h1>
+	)
+
+let name = "Me"
+
+let url = "https://mimo.app/i/lori.png"
+
+let app = (
+	<div>
+	{header}, {name}
+	{subheader}
+	</div>
+	<img src={url} alt="Me">
+	)
+
 ReactDOM.render(
-	<h1>Hello</h1>, document.getElementById("root")
+	app, document.getElementById("root")
 	)
